ab0d2d11ac1eda0c5bc48e272b71a5ec7b0e1e00
mozukraken <mozukraken@gmail.com>
2018-02-02 22:58:09 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <title>Calender</title>
+    <link rel="stylesheet" href="/css/style.css">
+  </head>
+  <body>
+    <table>
+      <thead>
+        <tr>
+          <th><a href="">&laquo;</a></th>
+          <th colspan="5">2/2</th>
+          <th><a href="">&raquo;</a></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Sun</td>
+          <td>Mon</td>
+          <td>Tue</td>
+          <td>Wed</td>
+          <td>Thu</td>
+          <td>Fri</td>
+          <td>Sat</td>
+        </tr>
+        <tr>
+          <td class="youbi_0">1</td>
+          <td class="youbi_1">2</td>
+          <td class="youbi_2">3</td>
+          <td class="youbi_3">4</td>
+          <td class="youbi_4 today">5</td>
+          <td class="youbi_5">6</td>
+          <td class="youbi_6">7</td>
+        </tr>
+        <tr>
+          <td class="youbi_0">30</td>
+          <td class="youbi_1">31</td>
+          <td class="gray">1</td>
+          <td class="gray">2</td>
+          <td class="gray">3</td>
+          <td class="gray">4</td>
+          <td class="gray">5</td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <tr>
+          <th colspan="7">Today</th>
+        </tr>
+      </tfoot>
+    </table>
+  </body>
+</html>
